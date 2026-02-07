b27c44115ea583fffa27b13ad50921c845c3beb6
Gabriel Matheus <133973057+GabrielMatheus1@users.noreply.github.com>
2026-02-07 13:55:59 -0300

    Add files via upload

---
+++ src/script/dados.js
@@ -0,0 +1,156 @@
+export const personagensHoraDeAventura = [
+   {
+       id: 1,
+       nome: 'Finn',
+       imgSrc: 'Finn.png',
+       descricao: 'Finn Mertens é um jovem guerreiro, presumivelmente o último humano vivo na Terra de Ooo que se aventura com seu melhor amigo e irmão adotivo Jake, o Cão. Finn é um herói disposto a tudo, que adora aventuras e não gosta de ver ninguém sofrer. Ele é o paladino jurado da Princesa Jujuba, soberana do Reino Doce, e tem a missão de protegê-la e o reino de ameaças.',
+       tipo: 'humano'
+   },
+   {
+       id: 2,
+       nome: 'Jake',
+       imgSrc: 'Jake.png',
+       descricao: 'Jake é um cão buldogue dourado, de quatro anos, que tem poderes mágicos, como se esticar mudar de forma e reorganizar os órgãos internos. Ele é o melhor amigo de Finn e toca violino com a namorada Lady Íris. Jake é um personagem carismático e engraçado, que sempre dá conselhos e informações.',
+       tipo: 'magico'
+   },
+   {
+       id: 3,
+       nome: 'BMO',
+       imgSrc: 'BMO.png',
+       descricao: 'BMO, um personagem do episódio de Hora de Aventura onde é apresentado como um videogame de Finn e Jake. BMO é capaz de falar, andar, dormir, se mexer e transportar pessoas para dentro de seus jogos.',
+       tipo: 'robo'
+   },
+   {
+       id: 4,
+       nome: 'Princesa Jujuba',
+       imgSrc: 'PrincesaJujuba.png',
+       descricao: 'Princesa Jujuba é a governante do Reino Doce. Embora não fale muito sobre seu passado sabe-se que muitos de seus parentes são famosos. Fica implícito que Finn salvou a Princesa muitas vezes, embora ela acaba salvando-o em alguns episódios.',
+       tipo: 'doce'
+   },
+   {
+       id: 5,
+       nome: 'Marceline',
+       imgSrc: 'Marceline.png',
+       descricao: 'Apesar de sua primeira aparição demonstrar uma natureza antagonista, com o passar da série a personagem se torna uma amiga próxima de Finn e Jake, e vai evoluindo sua personalidade e relacionamentos. Ao contrário dos vampiros convencionais, ela não precisa de sangue para viver, apenas se alimentando da cor vermelha. Ela também é uma grande cantora e compositora musical.',
+       tipo: 'vampiro'
+   },
+   {
+       id: 6,
+       nome: 'Princesa do Fogo',
+       imgSrc: 'PrincesaDoFogo.png',
+       descricao: 'Princesa de Fogo tem uma personalidade volátil, ela geralmente tem um comportamento calmo, embora com tendências bastantes frequentes a oscilações súbitas e violentas de humor.',
+       tipo: 'fogo'
+   },
+   {
+       id: 7,
+       nome: 'Princesa Caroço',
+       imgSrc: 'PrincesaCaroço.png',
+       descricao: 'A Princesa Caroço é uma princesa que flutua como nuvem e é da dimensão da Terra do Caroço, lugar habitado por pessoas "Encaroçadas". Ela é como as pessoas encaroçadas em geral. Ela é metida, rude e mal educada, e usa a palavra "caroço" no lugar de palavrões e as vezes a palavra "gosma" no lugar de expressões.',
+       tipo: 'caroco'
+   },
+   {
+       id: 8,
+       nome: 'Caracol',
+       imgSrc: 'OCaracol.png',
+       descricao: 'O Caracol aparece em todos os episódios (excluindo o Piloto, "The Wand" e "Cadeia Alimentar"). Ele aparece escondido nos lugares e objetos sem foco da câmera.',
+       tipo: 'animal'
+   },
+   {
+       id: 9,
+       nome: 'Rei Gelado',
+       imgSrc: 'ReiGelado.png',
+       descricao: 'Rei Gelado é um velho, solitário, mandante sociopata do Reino Gelado que é obcecado em capturar princesas na esperança de que uma delas seja sua esposa, até ameaçando-as de morte caso se recusem.',
+       tipo: 'magico'
+   },
+   {
+       id: 10,
+       nome: 'Lich',
+       imgSrc: 'Lich.png',
+       descricao: 'O Lich é uma Entidade Cósmica originada a partir do Cometa Catalisador que atingiu o planeta Terra durante o período cretáceo, reconhecido como a "personificação do mal", que tem como objetivo destruir toda a vida no Multiverso.',
+       tipo: 'entidade'
+   },
+   {
+       id: 11,
+       nome: 'GOLB',
+       imgSrc: 'GOLB.png',
+       descricao: 'GOLB é uma entidade cósmica maléfica, reconhecido como a personificação do caos, sendo considerado um dos seres mais poderosos do Universo.',
+       tipo: 'entidade'
+   },
+   {
+       id: 12,
+       nome: 'Limao Grab',
+       imgSrc: 'limao-grab.png',
+       descricao: 'Uma criação da Jujuba que deu errado; é extremamente temperamental, grita frases como "Inaceitável!" e governa o seu próprio reino de limões com punho de ferro.',
+       tipo: 'limao'
+   },
+   {
+       id: 13,
+       nome: 'Martin',
+       imgSrc: 'marti.png',
+       descricao: 'O pai biológico de Finn; um vigarista intergaláctico oportunista que foge de responsabilidades e vive se metendo em encrencas pelo cosmos.',
+       tipo: 'humano'
+   },
+   {
+       id: 14,
+       nome: 'Prisma',
+       imgSrc: 'prisma.png',
+       descricao: 'Uma entidade poderosa que vive na Câmara do Tempo; ele pode realizar qualquer desejo (com um preço) e adora relaxar, comer picles e bater papo com seus amigos.',
+       tipo: 'entidade'
+   },
+   {
+       id: 15,
+       nome: 'Maga Caçadora',
+       imgSrc: 'maga-cacadora.png',
+       descricao: 'Uma habilidosa e solitária lutadora que vive em sintonia com a natureza; ela possui uma conexão profunda com o mundo mágico e um interesse romântico por Finn.',
+       tipo: 'magico'
+   },
+   {
+       id: 16,
+       nome: 'Mordomo Menta',
+       imgSrc: 'mordomo.png',
+       descricao: 'O fiel e educado mordomo do Reino Doce que, secretamente, é um mestre das artes das trevas e possui conexões com forças ocultas e demoníacas.',
+       tipo: 'doce'
+   },
+   {
+       id: 17,
+       nome: 'chocoberry',
+       imgSrc: 'chocob.png',
+       descricao: 'Uma cidadã do Reino Doce que se assemelha a um morango coberto de chocolate; ela é conhecida por ser doce, mas um tanto nervosa e sensível.',
+       tipo: 'doce'
+   },
+   {
+       id: 18,
+       nome: 'Dona tromba',
+       imgSrc: 'tromba.png',
+       descricao: 'Uma elefantinha amarela e gentil, famosa por assar as melhores tortas de maçã de Ooo e por ter uma vida amorosa surpreendentemente agitada.',
+       tipo: 'animal' 
+   },
+   {
+       id: 19,
+       nome: 'Fionna',
+       imgSrc: 'fiona.png',
+       descricao: 'Fionna é a versão com gênero trocado do Finn. Assim como ele, é uma heroína corajosa e aventureira que usa um chapéu de coelho e carrega uma espada para combater o mal e proteger seus amigos.',
+       tipo: 'humano'
+   },
+   {
+       id: 20,
+       nome: 'Lady Iris',
+       imgSrc: 'iris.png',
+       descricao: 'É a leal companheira da Princesa Jujuba e namorada (posteriormente esposa) do Jake. Ela é uma criatura longa, com as cores do arco-íris, capaz de voar e atravessar paredes, e se comunica exclusivamente em coreano.',
+       tipo: 'iriscornio'
+   },
+   {
+       id: 21,
+       nome: 'Minerva',
+       imgSrc: 'minerv.png',
+       descricao: 'É a mãe biológica de Finn. Ela era uma "Ajudante" (médica) na Ilha dos Fundadores e, para proteger o que restava da humanidade, transferiu sua consciência para uma rede de robôs altruístas que cuidam da população.',
+       tipo: 'humano'
+   },
+   {
+       id: 22,
+       nome: 'Rei de Fogo',
+       imgSrc: 'reifogo.png',
+       descricao: 'Antigo governante do Reino de Fogo e pai da Princesa de Fogo. Ele é um monarca rigoroso e instável, cujo corpo é composto inteiramente de chamas vivas contidas em uma armadura real.',
+       tipo: 'fogo'
+   }
+];
